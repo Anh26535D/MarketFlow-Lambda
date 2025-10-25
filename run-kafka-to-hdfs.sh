883fab67b36ebaeafeda5017c1c833161ff43ed9
Anh26535D <86581717+Anh26535D@users.noreply.github.com>
2025-10-25 16:53:58 +0700

    Change to use apache/spark image
    - Remove permission in hdfs

--- run-kafka-to-hdfs.sh
+++ run-kafka-to-hdfs.sh
@@ -8,23 +8,28 @@ CONTAINER_JAR_PATH="/opt/spark-jobs/$JAR_NAME"
 MAIN_CLASS="edu.hust.marketflow.consumer.SparkKafkaToHdfs"
 SPARK_MASTER_URL="spark://spark-master:7077"
 
-echo "[1/4] Checking fat JAR with Maven Shade Plugin..."
-
+echo "[1/5] Verifying JAR..."
 if [ ! -f "$JAR_PATH" ]; then
-  echo "❌ JAR not found at $JAR_PATH"
+  echo "❌ JAR not found. Please build the project first using ./build_project.sh"
   exit 1
 fi
 
-echo "[2/4] Copying JAR into Spark master container..."
+echo "[2/5] Copying JAR into Spark master container..."
 cp "$JAR_PATH" ./spark-jobs
 
-echo "[3/4] Submitting Spark Streaming job to cluster..."
+# TODO: consider another way for permissions
+echo "[3/5] Change HDFS permissions..."
+docker exec -it namenode bash -c "
+  hdfs dfs -chmod 777 /
+"
+
+echo "[4/5] Submitting Spark Streaming job to cluster..."
 docker exec -it "$SPARK_CONTAINER" bash -c "
-  /spark/bin/spark-submit \
+  /opt/spark/bin/spark-submit \
     --class $MAIN_CLASS \
     --master $SPARK_MASTER_URL \
     $CONTAINER_JAR_PATH
 "
 
-echo "[4/4] Spark Streaming job is now running!"
+echo "[5/5] Spark Streaming job is now running!"
 echo "👉 Check Spark Web UI at: http://localhost:8090"
--- run-producer.sh
+++ run-producer.sh
@@ -7,11 +7,11 @@ PROJECT_DIR="$(cd "$(dirname "$0")" && pwd)"
 JAR_PATH="$PROJECT_DIR/target/marketflow-lambda-1.0-SNAPSHOT-shaded.jar"
 MAIN_CLASS="edu.hust.marketflow.producer.KafkaPriceProducer"
 
-# Check if built JAR exists
+echo "[1/2] Verifying JAR..."
 if [ ! -f "$JAR_PATH" ]; then
   echo "❌ JAR not found. Please build the project first using ./build_project.sh"
   exit 1
 fi
 
-echo "🚀 Running Kafka Producer locally..."
+echo "[2/2] Running Kafka Producer..."
 java -cp "$JAR_PATH" "$MAIN_CLASS"
